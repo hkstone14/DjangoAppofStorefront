45ca83d0df7f2ba4d017805e16f7eb848627df57
harikrushna <hp52@njit.edu>
2021-07-30 14:38:36 -0400

    Update : Two apps added (Store and Tags)

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # returning html templates on user request
 def say_hello(request):
     return render(request, 'hello.html')

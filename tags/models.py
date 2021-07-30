2dd64596ef351d74c056576dcb9035d325c717dd
harikrushna <hp52@njit.edu>
2021-07-30 16:11:45 -0400

    Update : New app 'likes' added

--- tags/models.py
+++ tags/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Tag(models.Model):
@@ -7,3 +9,8 @@ class Tag(models.Model):
 
 class TaggedItem(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+

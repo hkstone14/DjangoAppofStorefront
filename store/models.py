57b35a5d55b6dc80ad5d35c449633232a25695c7
harikrushna <hp52@njit.edu>
2021-07-30 15:58:39 -0400

    Update : Tag and TaggedItem classes added in tag app

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):

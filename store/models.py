17b4098ed6d8267fb990d0f610830a863a3dda40
harikrushna <hp52@njit.edu>
2021-07-30 15:22:52 -0400

    Update : Customer class added in Store app

--- store/models.py
+++ store/models.py
@@ -9,4 +9,18 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
 
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Sliver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)

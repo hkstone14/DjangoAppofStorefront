8616fd66399c13c42557b6e72676f45b253342b1
harikrushna <hp52@njit.edu>
2021-08-23 18:53:32 -0400

    Update : Custom list added

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
+admin.site.register(models.Product, ProductAdmin)

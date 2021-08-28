6ecd8faad112b5a2129934a9b5adf74f7389c91a
harikrushna <hp52@njit.edu>
2021-08-28 17:08:25 -0400

    Update : Customer and Product model added to admin site

--- playground/views.py
+++ playground/views.py
@@ -62,7 +62,7 @@ def say_hello(request):
     collection.featured_product = None
     collection.save()
 
-    # Deleting a single object from DB
+    # Deleting a single
     collection = Collection(pk=11)
     collection.delete()
 
